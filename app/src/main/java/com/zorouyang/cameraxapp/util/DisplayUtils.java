eb294e8b4c6d632f44d395c5318ed2f07e1d80cc
ZorOuyang <zorouyang@163.com>
2019-07-11 10:17:39 +0800

    Virtual Navigation Keyboard Full Screen

--- app/src/main/java/com/zorouyang/cameraxapp/util/DisplayUtils.java
+++ app/src/main/java/com/zorouyang/cameraxapp/util/DisplayUtils.java
@@ -67,7 +67,12 @@ public class DisplayUtils {
 
     public static void setStableFullscreen(Activity activity) {
         Window window = activity.getWindow();
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
         window.setStatusBarColor(Color.TRANSPARENT);
+        window.setNavigationBarColor(Color.TRANSPARENT);
+        //window.setNavigationBarColor(Color.parseColor("#40000000"));
     }
 }
